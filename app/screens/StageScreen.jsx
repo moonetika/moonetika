37845710eeb0d6803ce6ca58a1b19c674e634b98
Kevin Cabezas <kcabezas@fandom.com>
2024-10-18 21:25:33 +0200

    Basic changes to image save profile

--- app/screens/StageScreen.jsx
+++ app/screens/StageScreen.jsx
@@ -44,98 +44,13 @@ const ProfilePage = () => {
     router.push("screens/Profile");
   };
 
-  const pickImage = async () => {
-    // Ask for permission to access the media library
-    const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
-    if (status !== "granted") {
-      Alert.alert(
-        "Permission Denied",
-        "Sorry, we need camera roll permissions to make this work!"
-      );
-      return;
-    }
-
-    // Open the image picker
-    const result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.Images,
-      allowsEditing: true,
-      aspect: [1, 1],
-      quality: 1,
-    });
-
-    if (!result.canceled) {
-      const { uri } = result.assets[0]; // Get image URI
-      const savedUri = await saveImageToFileSystem(uri); // Save the image locally
-      console.log(savedUri, "savedUri");
-      setAvatar(savedUri); // Set the new av6atar
-    }
-    // if (!result.canceled) {
-    //   const { uri } = result.assets[0]; // Get image URI
-    //   await uploadImageToFirebase(uri); // Upload image to Firebase
-    // }
-  };
-
-  const SetUserProfileInfo = async (data, uid) => {
-    try {
-      await setDoc(doc(db, "cliente", uid), data);
-      setIsLoading(false);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const saveImageToFileSystem = async (uri) => {
-    try {
-      console.log(uri);
-      const fileName = uri.split("/").pop(); // Extract the file name
-      const localUri = `${FileSystem.documentDirectory}${fileName}`; // Define local path
-      await FileSystem.copyAsync({
-        from: uri,
-        to: localUri,
-      });
-      const userProfileMerged = { ...usuario, pictureFileName: fileName };
-      SetUserProfileInfo(userProfileMerged, loggedInUser.uid);
-      const finalUser = { ...loggedInUser, profile: userProfileMerged };
-      setLoggedInUser(finalUser);
-      return localUri; // Return the local path to the image
-    } catch (error) {
-      console.error("Error saving image:", error);
-    }
-  };
-
-  const uploadImageToFirebase = async (uri) => {
-    try {
-      setUploading(true);
-      const response = await fetch(uri);
-      const blob = await response.blob();
-      const fileName = uri.split("/").pop();
-      const ref = storage.ref().child(`avatars/${fileName}`); // Reference to where the image will be stored
-
-      // Upload the file to Firebase
-      const snapshot = await ref.put(blob);
-
-      // Get the download URL of the uploaded image
-      const downloadURL = await snapshot.ref.getDownloadURL();
-      console.log("New avatar URL:", downloadURL);
-      setAvatar(downloadURL); // Set the new avatar URL
-
-      Alert.alert("Success", "Your profile picture has been updated!");
-    } catch (error) {
-      console.error("Error uploading image: ", error);
-      Alert.alert("Upload failed", "There was an issue uploading the image.");
-    } finally {
-      setUploading(false);
-    }
-  };
-
-  useEffect(() => {
-    if (loggedInUser) {
-      console.log("basic");
-      setAvatar(
-        `${FileSystem.documentDirectory}${loggedInUser?.profile?.pictureFileName}`
-      );
-    }
-  }, []);
+  // useEffect(() => {
+  //   if (loggedInUser) {
+  //     setAvatar(
+  //       `${FileSystem.documentDirectory}${loggedInUser?.profile?.pictureFileName}`
+  //     );
+  //   }
+  // }, []);
 
   useEffect(() => {
     if (loggedInUser) {
@@ -166,12 +81,6 @@ const ProfilePage = () => {
             source={{ uri: "https://via.placeholder.com/120" }}
           />
         )}
-        <IconButton
-          icon="camera"
-          size={30}
-          onPress={pickImage}
-          disabled={uploading}
-        />
         <Title style={styles.username}>
           {usuario.name} {usuario.lastname}
         </Title>
@@ -197,8 +106,10 @@ const ProfilePage = () => {
 
           <Text style={styles.textLabel}> Direccion: </Text>
           <Text style={styles.textInfo}>
-            {usuario.streetName} {usuario.streetFloor} - {usuario.streetDoor},{" "}
-            {usuario.postalCode} {usuario.province}
+            {usuario.streetName} {usuario.streetDoorNumber}, {usuario.streetDoor} {usuario.postalCode}9+
+          </Text>
+          <Text style={styles.textInfo}>
+            {usuario.province} - {usuario.localidad}
           </Text>
 
           <Button
